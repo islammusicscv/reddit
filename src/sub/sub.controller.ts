e284593111536f2702ec68863fb2f81a6049ce50
Islam Mušić <islam.music@scv.si>
2024-04-12 12:00:48 +0200

    konec subreditov

--- src/sub/sub.controller.ts
+++ src/sub/sub.controller.ts
@@ -1,26 +1,29 @@
-import {Controller, Delete, Get, Patch, Post} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
+import {SubService} from "./sub.service";
+import {CreateSubDto} from "./create-sub.dto";
+import {SubEntity} from "./sub.entity";
 
-@Controller('sub')
+@Controller('subs')
 export class SubController {
 
+    constructor(private subService: SubService) {
+    }
+
     @Post()
-    createSub() {
-
+    createSub(@Body() createSub:CreateSubDto):Promise<SubEntity> {
+        return this.subService.create(createSub);
     }
-
     @Get()
-    readAll() {
-
+    readAll():Promise<SubEntity[]> {
+        return this.subService.readAll();
     }
-
-    @Patch()
-    updateSub() {
-
+    @Patch(':id')
+    updateSub(@Param(':id') id:number,@Body() updateSub:CreateSubDto) {
+        this.subService.update(id, updateSub);
     }
-
-    @Delete()
-    deleteSub() {
-
+    @Delete(':id')
+    deleteSub(@Param(':id') id:number) {
+        this.subService.delete(id);
     }
 
 }
--- src/sub/sub.service.ts
+++ src/sub/sub.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {SubEntity} from "./sub.entity";
+import {DeleteResult, Repository} from "typeorm";
+import {CreateSubDto} from "./create-sub.dto";
 
 @Injectable()
-export class SubService {}
+export class SubService {
+    constructor(@InjectRepository(SubEntity) private readonly subRepository:Repository<SubEntity>) {
+    }
+
+    async create(createSub: CreateSubDto):Promise<SubEntity> {
+        const user_id=1;
+        const data = {...createSub,user:{id:user_id}};
+        const sub = this.subRepository.create(data);
+        return await this.subRepository.save(sub);
+    }
+
+    async readAll():Promise<SubEntity[]> {
+        return await this.subRepository.find();
+    }
+
+    delete(id:number):Promise<DeleteResult> {
+        return this.subRepository.delete(id);
+    }
+
+    async update(id:number, updateSub:CreateSubDto) {
+        await this.subRepository.update(id,updateSub);
+        return this.subRepository.findOneBy({id});
+    }
+}
