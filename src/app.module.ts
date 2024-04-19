c2166965a7d20678f8a9d0c268466cbd51f81c03
Islam Mušić <islam.music@scv.si>
2024-04-19 11:02:09 +0200

    začetek auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import { ArticleModule } from './article/article.module';
 import {ConfigModule} from "@nestjs/config";
 import { SubModule } from './sub/sub.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -23,7 +24,8 @@ import { SubModule } from './sub/sub.module';
     }),
     UserModule,
     ArticleModule,
-    SubModule],
+    SubModule,
+    AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
